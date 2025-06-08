fba52d6da5f099c477ca0cafa136992c08f76080
Muktha <115140893+Smuktha@users.noreply.github.com>
2025-06-08 12:50:27 +0530

    Update cartContext.tsx

--- context/cartContext.tsx
+++ context/cartContext.tsx
@@ -8,7 +8,7 @@ import { Product } from "../types/product";
 interface CartItem {  
   product: Product;     
   quantity: number;   
-} 
+}    
 
 // Context type definition
 interface CartContextType {
