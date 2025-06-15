204ba893db40873466f5f6d573cec03878e4999a
Muktha <115140893+Smuktha@users.noreply.github.com>
2025-06-15 15:58:18 +0530

    Update Cart.tsx

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -1,6 +1,6 @@
 // components/Cart.tsx
 "use client";
-import React from "react";
+import React from "react"; 
 import { useCart } from "../context/cartContext";
      
 const Cart: React.FC = () => {
