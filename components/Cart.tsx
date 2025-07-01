467840a480e48d83a28081c1f65824e8d05c385f
Muktha <115140893+Smuktha@users.noreply.github.com>
2025-07-01 19:22:43 +0530

    Update Cart.tsx

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { useCart } from "../context/cartContext";
      
 const Cart: React.FC = () => {
-  const { cart, clearCart } = useCart();  
+  const { cart, clearCart } = useCart();   
 
   const handleClearCart = () => {
     clearCart();
