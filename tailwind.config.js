633223a843b0ae03337feeb9cb2c29098674bd77
Muktha <115140893+Smuktha@users.noreply.github.com>
2025-07-07 07:58:20 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     content: [ 
       "./pages/**/*.{js,ts,jsx,tsx}",
       "./components/**/*.{js,ts,jsx,tsx}",
-      // Add any other paths where Tailwind classes might be used 
+      // Add any other paths where Tailwind classes might be used  
     ], 
     theme: {
       extend: {},
